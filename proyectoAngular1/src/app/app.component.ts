a35fccd2dfab62738801849cd63e4db9592368a5
Valeria Foglino <valeriafoglino.78@gmail.com>
2023-07-19 15:11:48 -0300

    Modificando browser angular. Aprendiendo.

--- proyectoAngular1/src/app/app.component.ts
+++ proyectoAngular1/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyectoAngular1';
+  titulo = 'Proyecto de Angular 1';
+  subtitulo= 'Probando el subtitulo'
 }
